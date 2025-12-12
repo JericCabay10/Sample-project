70337bad0846a283ef3cfeb379824d3ebd4cbecd
Jeric Cabay <jericcabay10@gmail.com>
2025-12-12 09:36:48 +0800

    Update

--- src/main/java/com/example/project/model/Order.java
+++ src/main/java/com/example/project/model/Order.java
@@ -17,31 +17,15 @@ public class Order {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public boolean isBillBySomeoneElse() {
         return billBySomeoneElse;
-    }
-
-    public void setBillBySomeoneElse(boolean billBySomeoneElse) {
-        this.billBySomeoneElse = billBySomeoneElse;
     }
 }
